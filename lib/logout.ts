0636469ccf00f6829e3aab9e6929273982af1cf2
Kajan <kajanm2006@gmail.com>
2025-03-08 23:37:29 +0100

    fjhak

--- lib/logout.ts
+++ lib/logout.ts
@@ -1,3 +1,13 @@
 "use server"
 
 import { signOut } from "@/auth"
+
+export const logOut = async () => {
+    //Possibilité de faire un redirection
+    //    await signOut({
+    //      redirectTo: '/sign-in'
+    //})
+    await signOut({
+        redirectTo: '/sign-in'
+    })
+}
